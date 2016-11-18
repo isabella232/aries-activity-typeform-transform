679f992d6a7b45afb9a3d5d68c1ce3122fdd5b96
trong-le <trong.p.le.92@gmail.com>
2016-11-18 12:28:41 -0500

    Added test files and tests

--- test/test.js
+++ test/test.js
@@ -1,20 +1,37 @@
 import test from 'blue-tape';
-import TypeFormSource from '../lib/index.js';
+import TypeformTransform from '../lib/index.js';
 import config from './test-config';
 import fs from 'fs';
 import _ from 'highland';
+import omit from 'lodash.omit';
+import responseObject from './fixtures/testData.json';
+import mappedArray from './fixtures/mapped.json';
 
 test('proper configuration', t => {
-    const activity = new TypeFormSource();
-    t.equal(TypeFormSource.props.name, require('../package.json').name);
-    t.equal(TypeFormSource.props.version, require('../package.json').version);
+    const activity = new TypeformTransform();
+    t.equal(TypeformTransform.props.name, require('../package.json').name);
+    t.equal(TypeformTransform.props.version, require('../package.json').version);
     t.end();
 });
 
-test('test data', async (t) => {
-	const source = new TypeFormSource();
-	const stream = fs.createReadStream('test/fixtures/testData.json');
-	const highlandStream = _(stream).split().map(JSON.parse).errors(err => {});
-	const data = await source.handleStream(highlandStream);
-	t.comment(JSON.stringify(data));
+test('Array of mapped questions and answers', t => {
+    const source = new TypeformTransform();
+    const responses = source.getResponses(responseObject);
+    const mapped = responses.map((obj) => omit(obj, 'timestamp'));
+    t.equal(JSON.stringify(mapped), JSON.stringify(mappedArray));
+    t.end();
+});
+
+test('Map individual answer to question from array', t => {
+    const source = new TypeformTransform();
+    const answer = {
+        listimage_23566421_choice: 'Female'
+    }
+    const mappedQuestionAnswer = source.mapAnswerWithQuestions(answer, responseObject.questions);
+    const omittedResponse = omit(mappedQuestionAnswer, 'timestamp');
+    const expectedResponse = {
+        'What is your gender?': 'Female'
+    };
+    t.equal(JSON.stringify(omittedResponse), JSON.stringify(expectedResponse));
+    t.end();
 });
